91126ea954311e18158627417687f6be67876f83
Lotus Fenn <lotus.fenn@alumni.ubc.ca>
2016-11-03 19:08:32 -0700

    Updated NgSpiceTest, was meant to have it in an earlier commit

--- app/src/androidTest/java/com/cpen321/circuitsolver/ngspice/NgSpiceTest.java
+++ app/src/androidTest/java/com/cpen321/circuitsolver/ngspice/NgSpiceTest.java
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnitRunner;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
@@ -17,21 +18,33 @@ import static junit.framework.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class NgSpiceTest extends AndroidJUnitRunner {
 
+    private static String input = "* bla circuit\n" +
+            "v1 1 0 dc 24\n" +
+            "v2 3 0 dc 15\n" +
+            "r1 1 2 10k\n" +
+            "r2 2 3 8.1k\n" +
+            "r3 2 0 4.7k\n" +
+            ".CONTROL\n" +
+            "tran 1ns 1ns\n" +
+            ".ENDC\n" +
+            ".END";
+    private static String expected = "Node                                   Voltage\n" +
+            "----                                   -------\n" +
+            "1                                           24\n" +
+            "3                                           15\n" +
+            "2                                      9.74697\n" +
+            "v2#branch                         -0.000648522\n" +
+            "v1#branch                           -0.0014253\n";
+    @Before
+    public void initialize() {
+        Looper.prepare();
+    }
+
     @Test
     public void callNgSpiceTest() {
-        Looper.prepare();
         NgSpice ngSpice = NgSpice.getInstance(InstrumentationRegistry.getTargetContext());
-        String input = "* bla circuit\n" +
-                        "v1 1 0 dc 24\n" +
-                        "v2 3 0 dc 15\n" +
-                        "r1 1 2 10k\n" +
-                        "r2 2 3 8.1k\n" +
-                        "r3 2 0 4.7k\n" +
-                        ".CONTROL\n" +
-                        "tran 1ns 1ns\n" +
-                        ".ENDC\n" +
-                        ".END";
         String output = ngSpice.callNgSpice(input);
+        assertTrue(output.contains(expected));
     }
 
 }
