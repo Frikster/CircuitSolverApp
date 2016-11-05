90ffd682263f2bd9a485b020e7e4998add2dc611
kgoossen <kgoossen@alumni.ubc.ca>
2016-11-05 14:35:35 -0700

    Adding missing constants

--- app/src/main/java/com/cpen321/circuitsolver/util/Constants.java
+++ app/src/main/java/com/cpen321/circuitsolver/util/Constants.java
@@ -12,7 +12,17 @@ public final class Constants {
 
     public static final int PROCESSING_WIDTH = 125;
     public static final String RESISTOR = "r";
+    public static final String INDUCTOR = "l";
+    public static final String CAPACITOR = "c";
     public static final String WIRE = "w";
     public static final String DC_VOLTAGE = "v";
+
+    // unit types
+    public static final String RESISTOR_UNITS = "ohms";
+    public static final String INDUCTOR_UNTIS = "henrys";
+    public static final String CAPACITOR_UNITS = "farads";
+    public static final String VOLTAGE_UNITS = "volts";
+    public static final String NOTHING_SELECTED = "N/A";
+
 
 }
