db1e9cfd4b5c88b8efaa27e6e4121710bab49872
Lotus Fenn <lotus.fenn@alumni.ubc.ca>
2016-11-06 14:10:11 -0800

    Combined ngspice parser class into SpiceInterfacer class

--- app/src/main/java/com/cpen321/circuitsolver/ngspice/SpiceInterfacer.java
+++ app/src/main/java/com/cpen321/circuitsolver/ngspice/SpiceInterfacer.java
@@ -8,6 +8,7 @@ import com.cpen321.circuitsolver.model.components.CircuitElm;
 import com.cpen321.circuitsolver.util.Constants;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lotus on 05/11/16.
@@ -30,8 +31,71 @@ public class SpiceInterfacer {
         this.elements = elements;
     }
 
+
     public String getNgSpiceInput() {
         return addControl(createNetlist());
+    }
+
+    public void callNgOutputParser(String input, Map<String, CircuitNode> nodes, Map<String, CircuitElm> elms ) {
+        boolean addWord = false;
+        boolean isValueNum = false;
+        boolean isNode = false;
+        String key = null;
+
+        //replace all "\n" with spaces
+        String s = input;
+        s = s.replaceAll("\n", " ");
+        //split everything separated by spaces into new words
+        String[] arr = s.split(" ");
+
+        for ( String word : arr) {
+            //stop adding words after "No." word
+            if(word.equals("No.")) {
+                addWord = false;
+            }
+            //use this word
+            if(addWord && !word.equals("") && word != null) {
+                //node or element name, not a value
+                if (!isValueNum) {
+                    Log.v("Name of element or node", word);
+                    //next word will be a value number
+                    isValueNum = true;
+                    //get the key for the Map
+                    key = word;
+                    //is a branch
+                    if(word.length() != 0 && word.charAt(0)!= 'v'){
+                        isNode = true;
+                    }
+                    //is a node
+                    else{
+                        isNode = false;
+                    }
+                }
+                //is a voltage or current value
+                else {
+                    Log.v("Value to add", word );
+                    //next word is not a value
+                    isValueNum = false;
+                    //add voltage to node
+                    if(isNode){
+                        CircuitNode currNode = nodes.get(key);
+                        double value = Double.parseDouble(word);
+                        if(currNode != null)
+                            currNode.setVoltage(value);
+                    }
+                    //add current to element
+                    else {
+                        CircuitElm currElm = elms.get(key);
+                        if(currElm != null)
+                            currElm.setCurrent(Double.parseDouble(word));
+                    }
+                }
+            }
+            //Start adding words after word "-------"
+            if(word.equals("-------")){
+                addWord = true;
+            }
+        }
     }
 
     /**
--- app/src/test/java/com/cpen321/circuitsolver/ngspice/SpiceInterfacerTest.java
+++ app/src/test/java/com/cpen321/circuitsolver/ngspice/SpiceInterfacerTest.java
@@ -1,5 +1,7 @@
 package com.cpen321.circuitsolver.ngspice;
 
+import android.util.Log;
+
 import com.cpen321.circuitsolver.model.CircuitNode;
 import com.cpen321.circuitsolver.model.SimplePoint;
 import com.cpen321.circuitsolver.model.components.CircuitElm;
@@ -11,7 +13,11 @@ import com.cpen321.circuitsolver.service.AnalyzeCircuitImpl;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -51,11 +57,11 @@ public class SpiceInterfacerTest {
         System.out.println(ngSpiceInput);
 
         String expected = "* My Circuit\n" +
-                "v1 1 0 dc 24.0\n" +
-                "r1 1 2 10000.0\n" +
-                "r2 2 3 8100.0\n" +
-                "v2 3 0 dc 15.0\n" +
-                "r3 2 0 4700.0\n" +
+                "v1 4 3 dc 24.0\n" +
+                "r3 4 5 10000.0\n" +
+                "r4 5 6 8100.0\n" +
+                "v2 6 3 dc 15.0\n" +
+                "r5 5 3 4700.0\n" +
                 "\n" +
                 ".CONTROL\n" +
                 "tran 1ns 1ns\n" +
@@ -63,9 +69,61 @@ public class SpiceInterfacerTest {
                 ".END";
 
         assertTrue(ngSpiceInput.contains(expected));
+    }
 
+    @Test
+    public void callNgOutputParserTest() {
+        String input =
+                "Doing analysis at TEMP = 27.000000 and TNOM = 27.000000\n" +
+                        "\n" +
+                        "Initial Transient Solution\n" +
+                        "--------------------------\n" +
+                        "Node                                   Voltage\n" +
+                        "----                                   -------\n" +
+                        "1                                           24\n" +
+                        "3                                           15\n" +
+                        "2                                      9.74697\n" +
+                        "v2#branch                         -0.000648522\n" +
+                        "v1#branch                           -0.0014253\n" +
+                        "No. of Data Rows : 59\n";
 
+        SimplePoint f = new SimplePoint(100,200);//arb inputs
+        SimplePoint g = new SimplePoint(100,200);//arb inputs
 
+        final CircuitNode node1 = new CircuitNode();
+        final CircuitNode node2 = new CircuitNode();
+        final CircuitNode node3 = new CircuitNode();
+        // represents the v1#branch
+        final CircuitElm elm1 = new ResistorElm(g, f, 1.0);
+        //represents the v2#branch
+        final CircuitElm elm2 = new ResistorElm(f, g, 3.0);
+
+        //initialize map of Strings of nodeNum to a CircuitNode
+        Map<String, CircuitNode> nodes = new HashMap<String, CircuitNode>() {{
+            put("1",node1);
+            put("2", node2);
+            put("3", node3);
+        }};
+        //initialize map of Strings of a branch to a CircuitElm
+        Map<String, CircuitElm> elms = new HashMap<String, CircuitElm>() {{
+            put("v1#branch",elm1);
+            put("v2#branch",elm2);
+        }};
+
+        SpiceInterfacer spiceInterfacer = new SpiceInterfacer(null);
+
+        spiceInterfacer.callNgOutputParser(input, nodes, elms);
+        Log.v("node1 voltage", String.valueOf(node1.getVoltage()));
+        Log.v("node2 voltage", String.valueOf(node2.getVoltage()));
+        Log.v("node3 voltage", String.valueOf(node3.getVoltage()));
+        Log.v("v1#branch", String.valueOf(elm1.getCurrent()));
+        Log.v("v2#branch", String.valueOf(elm2.getCurrent()));
+
+        assertEquals(node1.getVoltage(),24.0);
+        assertEquals(node2.getVoltage(),9.74697);
+        assertEquals(node3.getVoltage(),15.0);
+        assertEquals(elm1.getCurrent(),-0.0014253);
+        assertEquals(elm2.getCurrent(),-0.000648522);
     }
 
 
