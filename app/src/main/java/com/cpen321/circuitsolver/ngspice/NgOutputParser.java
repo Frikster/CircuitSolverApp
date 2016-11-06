c8cb37baa0dae66b7eaf14ca7c4a0b1a859229da
Bruce Znang <ilovecandy221@yahoo.ca>
2016-11-05 22:43:42 -0700

    parser

---
+++ app/src/main/java/com/cpen321/circuitsolver/ngspice/NgOutputParser.java
@@ -0,0 +1,82 @@
+package com.cpen321.circuitsolver.ngspice;
+
+import android.util.Log;
+
+import com.cpen321.circuitsolver.model.CircuitNode;
+import com.cpen321.circuitsolver.model.components.CircuitElm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Bruce on 2016-11-05.
+ */
+
+public class NgOutputParser {
+
+    public static void callNgOutputParser(String input, Map nodes, Map elms ) {
+        boolean addWord = false;
+        boolean isValueNum = false;
+        boolean isNode = false;
+        String key = null;
+
+        //nodes.put("1",cicuitNode);
+
+        //replace all "\n" with spaces
+        String s = input;
+        s = s.replaceAll("\n", " ");
+        //split everything separated by spaces into new words
+        String[] arr = s.split(" ");
+
+        for ( String word : arr) {
+            //stop adding words after "No." word
+            if(word.equals("No.")) {
+                addWord = false;
+            }
+            //use this word
+            if(addWord && !word.equals("") && word != null) {
+                //node or element name, not a value
+                if (!isValueNum) {
+                    Log.v("Name of element or node", word);
+                    //next word will be a value number
+                    isValueNum = true;
+                    //get the key for the Map
+                    key = word;
+                    //is a branch
+                    if(word.length() != 0 && word.charAt(0)!= 'v'){
+                        isNode = true;
+                    }
+                    //is a node
+                    else{
+                        isNode = false;
+                    }
+                }
+                //is a voltage or current value
+                else {
+                    Log.v("Value to add", word );
+                    //next word is not a value
+                    isValueNum = false;
+                    //add voltage to node
+                    if(isNode){
+                        CircuitNode currNode = (CircuitNode)nodes.get(key);
+                        double value = Double.parseDouble(word);
+                        if(currNode != null)
+                            currNode.setVoltage(value);
+                    }
+                    //add current to element
+                    else {
+                        CircuitElm currElm = (CircuitElm)elms.get(key);
+                        if(currElm != null)
+                            currElm.setCurrent(Double.parseDouble(word));
+                    }
+                }
+            }
+            //Start adding words after word "-------"
+            if(word.equals("-------")){
+                addWord = true;
+            }
+        }
+
+    }
+}
